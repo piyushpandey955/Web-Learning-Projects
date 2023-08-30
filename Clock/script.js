c77a4964868903a8b3ef7e48a07a26c1fedf8a5b
Piyush Pandey <piyush7838732397@gmail.com>
2023-08-30 14:26:41 +0530

    Update script.js

--- Clock/script.js
+++ Clock/script.js
@@ -1,17 +1,31 @@
-let date = new Date();
-let hour = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-let d = date.getDate();
-let day = date.getDay();
-let month = date.getMonth();
-let year = date.getFullYear();
+// let date = new Date();
+// let hour = date.getHours();
+// let min = date.getMinutes();
+// let sec = date.getSeconds();
+// let d = date.getDate();
+// let day = date.getDay();
+// let month = date.getMonth();
+// let year = date.getFullYear();
 
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+// let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-let week = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday", "Sunday"]
+// let week = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday", "Sunday"]
 
 let timeUpdate = () => {
+
+    let date = new Date();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let d = date.getDate();
+    let day = date.getDay();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    let week = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday", "Sunday"]
+
     let time = document.getElementsByClassName("time")[0];
     let datee = document.getElementsByClassName("date")[0];
 
